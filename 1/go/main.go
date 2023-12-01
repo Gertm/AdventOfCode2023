96a1ee4574f0e70647b34e81df0879e8e4dc40f4
Gert Meulyzer <gertm@Gerts-Air.fritz.box>
2023-12-01 13:43:12 +0100

    cleaned it up for part 2

--- 1/go/main.go
+++ 1/go/main.go
@@ -5,40 +5,39 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
-	log.Println("AoC 2023 - exercise 1")
-	input := readInput()
 	total := 0
-	for _, line := range input {
-		total = total + getCalVal(line)
-	}
-	fmt.Println(total)
-}
-
-func readInput() []string {
 	f, err := os.Open("../input")
 	if err != nil {
 		log.Fatal("Input file isn't there?", err.Error())
 	}
 	defer f.Close()
-	result := make([]string, 1000)
 	s := bufio.NewScanner(f)
 	s.Split(bufio.ScanLines)
 	for s.Scan() {
-		result = append(result, s.Text())
+		total = total + getCalVal(s.Text())
 	}
-	return result
+	fmt.Println(total)
 }
 
 func getCalVal(line string) int {
 	// On each line, the calibration value can be found by combining the first digit
 	// and the last digit (in that order) to form a single two-digit number
 	digits := []int{}
+	acc := ""
 	for _, c := range line {
 		if c >= 48 && c <= 57 {
+			acc = ""
 			digits = append(digits, int(c)-48)
+		} else {
+			acc += string(c)
+			if ok, val := containsNumber(acc); ok {
+				digits = append(digits, val)
+				acc = string(acc[len(acc)-1]) // keep the last char in case of 'oneight' etc.
+			}
 		}
 	}
 	l := len(digits)
@@ -46,5 +45,25 @@ func getCalVal(line string) int {
 		return int(digits[0]*10) + int(digits[l-1])
 	}
 	return 0
+}
 
+func containsNumber(s string) (bool, int) {
+	numbers := []string{
+		"zero",
+		"one",
+		"two",
+		"three",
+		"four",
+		"five",
+		"six",
+		"seven",
+		"eight",
+		"nine",
+	}
+	for i, n := range numbers {
+		if strings.Index(s, n) >= 0 {
+			return true, i
+		}
+	}
+	return false, -1
 }
--- 1/go/main_test.go
+++ 1/go/main_test.go
@@ -27,7 +27,7 @@ func Test_readInput(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := readInput(); !reflect.DeepEqual(got, tt.want) {
+			if got := processInput(); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("readInput() = %v, want %v", got, tt.want)
 			}
 		})
@@ -43,33 +43,52 @@ treb7uchet
 
 func Test_getCalVal(t *testing.T) {
 	tests := []struct {
-		name string
 		args string
 		want int
 	}{
 		{
 			"1abc2",
-			"1abc2",
 			12,
 		},
 		{
-			"2",
 			"pqr3stu8vwx",
 			38,
 		},
 		{
-			"3",
 			"a1b2c3d4e5f",
 			15,
 		},
 		{
-			"4",
 			"treb7uchet",
 			77,
 		},
+		{
+			"two1nine",
+			29,
+		},
+		{
+			"eightwothree",
+			83,
+		},
+		{
+			"abcone2threexyz",
+			13,
+		},
+		{
+			"xtwone3four",
+			24,
+		},
+		{
+			"4nineeightseven2",
+			42,
+		},
+		{
+			"wdfwoneightowdi",
+			18,
+		},
 	}
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
+		t.Run("aoc1", func(t *testing.T) {
 			if got := getCalVal(tt.args); got != tt.want {
 				t.Errorf("getCalVal() = %v, want %v", got, tt.want)
 			}
